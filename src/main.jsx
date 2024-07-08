700c21dac9d0345da6fad79de2ae8dd41f8a0958
Sathish0225 <kumar@a-sat.sg>
2024-07-08 19:59:25 +0800

    First Commit

---
+++ src/main.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import 'swiper/css';
+
+// bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+// fonts and icons
+import '././assets/css/icofont.min.css';
+import '././assets/css/animate.css';
+import '././assets/css/style.min.css';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from './home/Home.jsx';
+import Blog from './blog/Blog.jsx';
+import Shop from './shop/Shop.jsx';
+import About from './about/About.jsx';
+import Contact from './contact/Contact.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/shop", element: <Shop /> },
+      { path: "/blog", element: <Blog /> },
+      { path: "/about", element: <About /> },
+      { path: "/contact", element: <Contact /> },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
